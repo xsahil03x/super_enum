29fd9342f8c5e4f277abae37b3716546e1e8e603
Sahil Kumar <xdsahil@gmail.com>
2019-11-11 19:48:58 +0530

    Add factory constructors to mimic derived classes a enums
    Signed-off-by: Sahil Kumar <xdsahil@gmail.com>

---
+++ sealed_ugh_generator/lib/src/extension.dart
@@ -0,0 +1,44 @@
+String getCamelCase(String text, {String separator: ''}) {
+  List<String> words =
+      _groupIntoWords(text).map(_upperCaseFirstLetter).toList();
+  words[0] = words[0].toLowerCase();
+
+  return words.join(separator);
+}
+
+String _upperCaseFirstLetter(String word) {
+  return '${word.substring(0, 1).toUpperCase()}${word.substring(1).toLowerCase()}';
+}
+
+final RegExp _upperAlphaRegex = new RegExp(r'[A-Z]');
+final RegExp _symbolRegex = new RegExp(r'[ ./_\-]');
+
+List<String> _groupIntoWords(String text) {
+  StringBuffer sb = new StringBuffer();
+  List<String> words = [];
+  bool isAllCaps = !text.contains(RegExp('[a-z]'));
+
+  for (int i = 0; i < text.length; i++) {
+    String char = new String.fromCharCode(text.codeUnitAt(i));
+    String nextChar = (i + 1 == text.length
+        ? null
+        : new String.fromCharCode(text.codeUnitAt(i + 1)));
+
+    if (_symbolRegex.hasMatch(char)) {
+      continue;
+    }
+
+    sb.write(char);
+
+    bool isEndOfWord = nextChar == null ||
+        (_upperAlphaRegex.hasMatch(nextChar) && !isAllCaps) ||
+        _symbolRegex.hasMatch(nextChar);
+
+    if (isEndOfWord) {
+      words.add(sb.toString());
+      sb.clear();
+    }
+  }
+
+  return words;
+}
