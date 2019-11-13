0391744a0cef70c298073a68280f85cea92a6ba4
astralstriker <sachdeva.jai@outlook.com>
2019-11-13 15:26:22 +0530

    [Generator] Organization

--- sealed_generator/lib/src/class_generator.dart
+++ sealed_generator/lib/src/class_generator.dart
@@ -1,9 +1,8 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:code_builder/code_builder.dart';
 import 'package:sealed/sealed.dart';
-import 'package:sealed_generator/src/type_processor.dart';
+import 'package:sealed_generator/src/type_processor.dart' as TypeProcessor;
 import 'package:sealed_generator/src/extension.dart';
-import 'package:source_gen/source_gen.dart';
 import 'package:dart_style/dart_style.dart';
 import 'package:sealed_generator/src/references.dart' as References;
 
@@ -11,34 +10,37 @@ class ClassGenerator {
   final ClassElement element;
   const ClassGenerator(this.element);
 
-  List<FieldElement> get _fields => element.fields.skip(2).toList();
+  Iterable<FieldElement> get _fields => element.fields.skip(2);
 
-  bool get isNamespaceGeneric => element.fields.any(
-      (e) => TypeProcessor.typeChecker(Generic).hasAnnotationOfExact(e));
+  bool get isNamespaceGeneric => _fields.any(TypeProcessor.isGeneric);
 
   String generate(DartFormatter _dartFmt) {
     assert(element.isEnum);
     assert(element.isPrivate);
+  
+    try {
+      final cls = Class((c) => c
+        ..name = ('${element.name.replaceFirst('_', '')}')
+        ..annotations.add(References.immutable)
+        ..types.addAll(isNamespaceGeneric ? [References.generic_T] : [])
+        ..abstract = true
+        ..fields.add(Field((f) => f
+          ..name = '_type'
+          ..modifier = FieldModifier.final$
+          ..type = refer(element.name)
+          ..build()))
+        ..constructors.addAll(_generateClassConstructors)
+        ..methods.add(_generateWhenMethod)
+        ..build());
 
-    final cls = Class((c) => c
-      ..name = ('${element.name.replaceFirst('_', '')}')
-      ..annotations.add(References.immutable)
-      ..types.addAll(isNamespaceGeneric ? [References.generic_T] : [])
-      ..abstract = true
-      ..fields.add(Field((f) => f
-        ..name = '_type'
-        ..modifier = FieldModifier.final$
-        ..type = refer(element.name)
-        ..build()))
-      ..constructors.addAll(_generateClassConstructors)
-      ..methods.add(_generateWhenMethod)
-      ..build());
-    final emitter = DartEmitter();
-    return _dartFmt.format('${cls.accept(emitter)}$_generateDerivedClasses');
+      final emitter = DartEmitter();
+      return _dartFmt.format('${cls.accept(emitter)}$_generateDerivedClasses');
+    } catch (e) {
+      return "/*${e.stackTrace}*/";
+    }
   }
 
   Method get _generateWhenMethod {
-
     final List<Parameter> _params = [];
     final StringBuffer _bodyBuffer = StringBuffer();
 
@@ -70,55 +72,51 @@ class ClassGenerator {
   }
 
   Iterable<Constructor> get _generateClassConstructors => [
-      Constructor((constructor) => constructor
-        ..constant = true
-        ..requiredParameters.add(Parameter((p) => p
-          ..name = 'this._type'
-          ..build()))
-        ..build()),
-    ].followedBy(_fields.map((field) => Constructor((constructor) => constructor
-      ..factory = true
-      ..name = '${getCamelCase(field.name)}'
-      ..optionalParameters.addAll(
-          TypeProcessor.typeChecker(Data).hasAnnotationOfExact(field)
-              ? _generateClassConstructorFields(field)
-              : [])
-      ..redirect = refer('${field.name}${isNamespaceGeneric ? '<T>' : ''}')
-      ..build())));
+        Constructor((constructor) => constructor
+          ..constant = true
+          ..requiredParameters.add(Parameter((p) => p
+            ..name = 'this._type'
+            ..build()))
+          ..build()),
+      ].followedBy(_fields.map((field) => Constructor((constructor) =>
+          constructor
+            ..factory = true
+            ..name = '${getCamelCase(field.name)}'
+            ..optionalParameters.addAll(TypeProcessor.hasAnnotation<Data>(field)
+                ? _generateClassConstructorFields(field)
+                : [])
+            ..redirect =
+                refer('${field.name}${isNamespaceGeneric ? '<T>' : ''}')
+            ..build())));
 
   Iterable<Parameter> _generateClassConstructorFields(FieldElement element) {
-    final _annotation =
-        TypeProcessor.typeChecker(Data).firstAnnotationOfExact(element);
-    final _fields = ConstantReader(_annotation).read('fields').listValue;
-
-    return _fields.map((e) => Parameter((f) => f
-      ..name = '${ConstantReader(e).read('name').stringValue}'
-      ..type = TypeProcessor.typeOfParameter(e) != "Generic"
-          ? refer(TypeProcessor.typeOfParameter(e))
+    final fields = TypeProcessor.listTypeFieldOf<Data>(element, 'fields');
+    return fields.map((e) => Parameter((f) => f
+      ..name = '${TypeProcessor.dataFieldName(e)}'
+      ..type = TypeProcessor.datafieldType(e) != "Generic"
+          ? refer(TypeProcessor.datafieldType(e))
           : References.generic_T
       ..named = true
       ..annotations.add(References.required)
       ..build()));
   }
 
-  String get _generateDerivedClasses {
-    final fields = element.fields.skip(2);
-    final DartEmitter emitter = DartEmitter();
-    return fields.map((field) {
-      if (TypeProcessor.typeChecker(Object)
-          .hasAnnotationOfExact(field)) {
-        return '${_generateObjectClass(field).accept(emitter)}';
-      } else if (TypeProcessor.typeChecker(Data)
-          .hasAnnotationOfExact(field)) {
-        return '${_generateDataClass(field).accept(emitter)}';
-      } else {
-        return null;
-      }
-    }).where((e) => e != null).join('');
-  }
+  String get _generateDerivedClasses => _fields
+      .map((field) {
+        if (TypeProcessor.hasAnnotation<Object>(field)) {
+          return '${_generateObjectClass(field).accept(DartEmitter())}';
+        } else if (TypeProcessor.hasAnnotation<Data>(field)) {
+          return '${_generateDataClass(field).accept(DartEmitter())}';
+        } else {
+          return null;
+        }
+      })
+      .where((e) => e != null)
+      .join('');
 
   Class _generateObjectClass(FieldElement field) {
-    final isGeneric = TypeProcessor.typeChecker(Generic).hasAnnotationOfExact(field);
+    final isGeneric = TypeProcessor.isGeneric(field);
+    print('_generateObjectClass');
 
     if (isGeneric) throw 'Can\'t use @generic on object classes';
 
@@ -136,19 +134,17 @@ class ClassGenerator {
   }
 
   Class _generateDataClass(FieldElement field) {
-    final annotation = TypeProcessor.typeChecker(Data).firstAnnotationOfExact(field);
-    final isGeneric = TypeProcessor.typeChecker(Generic).hasAnnotationOfExact(field);
-
-    final _classFields =
-        ConstantReader(annotation).read('fields')?.listValue ?? [];
+    final _classFields = TypeProcessor.listTypeFieldOf<Data>(field, 'fields');
+    final isGeneric = TypeProcessor.isGeneric(field);
 
     if (isGeneric) {
-      if (_classFields.every((e) => TypeProcessor.typeOfParameter(e) != "Generic")) {
+      if (_classFields
+          .every((e) => TypeProcessor.datafieldType(e) != "Generic")) {
         throw '${field.name} must have atleast one Generic field';
       }
     }
 
-    if (_classFields.any((e) => TypeProcessor.typeOfParameter(e) == "Generic")) {
+    if (_classFields.any((e) => TypeProcessor.datafieldType(e) == "Generic")) {
       if (!isGeneric) throw '${field.name} must be annotated with @generic';
     }
 
@@ -159,17 +155,17 @@ class ClassGenerator {
       ..annotations.add(References.immutable)
       ..types.addAll(isNamespaceGeneric ? [References.generic_T] : [])
       ..fields.addAll(_classFields.map((e) => Field((f) => f
-        ..name = ConstantReader(e).read('name').stringValue
+        ..name = TypeProcessor.dataFieldName(e)
         ..modifier = FieldModifier.final$
-        ..type = TypeProcessor.typeOfParameter(e) != "Generic"
-            ? refer(TypeProcessor.typeOfParameter(e))
+        ..type = TypeProcessor.datafieldType(e) != "Generic"
+            ? refer(TypeProcessor.datafieldType(e))
             : References.generic_T
         ..build())))
       ..constructors.add(Constructor((constructor) => constructor
         ..constant = true
         ..initializers.add(Code('super(${element.name}.${field.name})'))
         ..optionalParameters.addAll(_classFields.map((e) => Parameter((f) => f
-          ..name = 'this.${ConstantReader(e).read('name').stringValue}'
+          ..name = 'this.${TypeProcessor.dataFieldName(e)}'
           ..named = true
           ..annotations.add(References.required)
           ..build())))
--- sealed_generator/lib/src/type_processor.dart
+++ sealed_generator/lib/src/type_processor.dart
@@ -1,7 +1,20 @@
 import 'package:source_gen/source_gen.dart';
+import 'package:analyzer/dart/element/element.dart';
+import 'package:sealed/sealed.dart';
 
-class TypeProcessor {
-  static TypeChecker typeChecker(Type t) => TypeChecker.fromRuntime(t);
-  static String typeOfParameter(obj) =>
-      ConstantReader(obj).read('type').typeValue.toString();
-}
+TypeChecker _typeChecker(Type t) => TypeChecker.fromRuntime(t);
+/////////////////////////////////
+String datafieldType(obj) =>
+    ConstantReader(obj).read('type').typeValue.toString();
+String dataFieldName(obj) => ConstantReader(obj).read('name').stringValue;
+/////////////////////////////////
+ConstantReader annotationOf<T>(obj) =>
+    ConstantReader(_typeChecker(T).firstAnnotationOfExact(obj));
+ConstantReader fieldOf<T>(obj, String fieldName) =>
+    annotationOf<T>(obj)?.read(fieldName);
+Iterable listTypeFieldOf<T>(obj, String fieldName) =>
+    fieldOf<T>(obj, fieldName)?.listValue ?? [];
+////////////////////////////////
+bool hasAnnotation<T>(obj) => _typeChecker(T).hasAnnotationOfExact(obj);
+bool isGeneric(Element element) =>
+    _typeChecker(Generic).hasAnnotationOfExact(element);
