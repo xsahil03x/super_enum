04c7e2a515b2431c568ba227d9d0355a54f58ba4
Pascal Welsch <pascal@welsch.dev>
2020-01-20 01:25:13 +0100

    Run coverage without test_coverage package
    test_coverage doesn't really work for unit tests which require async setup (like here, loading directories). It was pure luck that it worked until now where the future basically synchronously completed for a single loaded file. Did not work when adding more files/tests.
    
    Now the coverage script has been moved into the project, allowing async loading before registering tests

--- super_enum_generator/test/super_enum_generator_test.dart
+++ super_enum_generator/test/super_enum_generator_test.dart
@@ -1,14 +1,19 @@
+import 'package:source_gen/source_gen.dart';
 import 'package:source_gen_test/source_gen_test.dart';
 import 'package:super_enum/super_enum.dart';
 import 'package:super_enum_generator/src/generator.dart';
 
-Future<void> main() async {
-  initializeBuildLogTracking();
-  final generator_test_src = await initializeLibraryReaderForDirectory(
-      'test/src', 'generator_test_src.dart');
-  testAnnotatedElements<SuperEnum>(generator_test_src, SuperEnumGenerator());
+final test_src_files = [
+  'generator_test_src.dart',
+  'use_class_test_src.dart',
+];
 
-  final use_class_test = await initializeLibraryReaderForDirectory(
-      'test/src', 'use_class_test_src.dart');
-  testAnnotatedElements<SuperEnum>(use_class_test, SuperEnumGenerator());
+Future<void> main() async {
+  List<LibraryReader> test_src = await Future.wait(test_src_files
+      .map((it) => initializeLibraryReaderForDirectory('test/src', it)));
+  initializeBuildLogTracking();
+
+  for (final src in test_src) {
+    testAnnotatedElements<SuperEnum>(src, SuperEnumGenerator());
+  }
 }
