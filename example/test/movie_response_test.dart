650a522f23903b821d6caba37c62d8c757e78c68
astralstriker <sachdeva.jai@outlook.com>
2020-02-04 01:11:01 +0530

    Uncomment tests and remove unused code
    Signed-off-by: astralstriker <sachdeva.jai@outlook.com>

--- example/test/movie_response_test.dart
+++ example/test/movie_response_test.dart
@@ -1,90 +1,90 @@
-// import 'dart:convert';
-// import 'dart:io';
+import 'dart:convert';
+import 'dart:io';
 
-// import 'package:example/movie_response.dart';
-// import 'package:example/movies.dart';
-// import 'package:example/movies_fetcher.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mockito/mockito.dart';
-// import 'package:http/http.dart' as http;
+import 'package:example/movie_response.dart';
+import 'package:example/movies.dart';
+import 'package:example/movies_fetcher.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
 
-// class MockClient extends Mock implements http.Client {}
+class MockClient extends Mock implements http.Client {}
 
-// void main() {
-//   MockClient _mockClient;
-//   MoviesFetcher _fakeMovieFetcher;
-//   final _baseUrl = "http://api.themoviedb.org/3/movie";
+void main() {
+  MockClient _mockClient;
+  MoviesFetcher _fakeMovieFetcher;
+  final _baseUrl = "http://api.themoviedb.org/3/movie";
 
-//   setUpAll(() {
-//     _mockClient = MockClient();
-//     _fakeMovieFetcher = MoviesFetcher(
-//       client: _mockClient,
-//       apiKey: 'fakeApiKey',
-//     );
-//   });
+  setUpAll(() {
+    _mockClient = MockClient();
+    _fakeMovieFetcher = MoviesFetcher(
+      client: _mockClient,
+      apiKey: 'fakeApiKey',
+    );
+  });
 
-//   Future<String> _getMovieJson() async {
-//     final file = File('assets/movies.json');
-//     return await file.readAsString();
-//   }
+  Future<String> _getMovieJson() async {
+    final file = File('assets/movies.json');
+    return await file.readAsString();
+  }
 
-//   group('MovieResponse super enum test:', () {
-//     test('fetch movies should emit MovieResponse.success', () async {
-//       final moviesJson = await _getMovieJson();
-//       final movies = Movies.fromJson(json.decode(moviesJson));
+  group('MovieResponse super enum test:', () {
+    test('fetch movies should emit MovieResponse.success', () async {
+      final moviesJson = await _getMovieJson();
+      final movies = Movies.fromJson(json.decode(moviesJson));
 
-//       when(_mockClient.get(
-//         '$_baseUrl/popular?api_key=fakeApiKey',
-//       )).thenAnswer(
-//         (_) async => http.Response(moviesJson, 200, headers: {
-//           HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'
-//         }),
-//       );
+      when(_mockClient.get(
+        '$_baseUrl/popular?api_key=fakeApiKey',
+      )).thenAnswer(
+        (_) async => http.Response(moviesJson, 200, headers: {
+          HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'
+        }),
+      );
 
-//       _fakeMovieFetcher.fetchMovies().listen(expectAsync1((event) => expect(
-//             event,
-//             MoviesResponse.success(movies: movies),
-//           )));
-//     });
+      _fakeMovieFetcher.fetchMovies().listen(expectAsync1((event) => expect(
+            event,
+            MoviesResponse.success(movies: movies),
+          )));
+    });
 
-//     test('fetch movies should emit MovieResponse.unauthorized', () async {
-//       when(_mockClient.get(
-//         '$_baseUrl/popular?api_key=fakeApiKey',
-//       )).thenAnswer(
-//         (_) async => http.Response(await _getMovieJson(), 401, headers: {
-//           HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'
-//         }),
-//       );
+    test('fetch movies should emit MovieResponse.unauthorized', () async {
+      when(_mockClient.get(
+        '$_baseUrl/popular?api_key=fakeApiKey',
+      )).thenAnswer(
+        (_) async => http.Response(await _getMovieJson(), 401, headers: {
+          HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8'
+        }),
+      );
 
-//       _fakeMovieFetcher.fetchMovies().listen(expectAsync1((event) => expect(
-//             event,
-//             MoviesResponse.unauthorized(),
-//           )));
-//     });
+      _fakeMovieFetcher.fetchMovies().listen(expectAsync1((event) => expect(
+            event,
+            MoviesResponse.unauthorized(),
+          )));
+    });
 
-//     test('fetch movies should emit MovieResponse.noNetwork', () async {
-//       when(_mockClient.get(
-//         '$_baseUrl/popular?api_key=fakeApiKey',
-//       )).thenThrow(SocketException(''));
+    test('fetch movies should emit MovieResponse.noNetwork', () async {
+      when(_mockClient.get(
+        '$_baseUrl/popular?api_key=fakeApiKey',
+      )).thenThrow(SocketException(''));
 
-//       _fakeMovieFetcher.fetchMovies().listen(expectAsync1((event) => expect(
-//             event,
-//             MoviesResponse.noNetwork(),
-//           )));
-//     });
+      _fakeMovieFetcher.fetchMovies().listen(expectAsync1((event) => expect(
+            event,
+            MoviesResponse.noNetwork(),
+          )));
+    });
 
-//     test('fetch movies should emit MovieResponse.unexpectedException',
-//         () async {
-//       final exception = Exception('Unexpected Error Occured');
+    test('fetch movies should emit MovieResponse.unexpectedException',
+        () async {
+      final exception = Exception('Unexpected Error Occured');
 
-//       when(_mockClient.get(
-//         '$_baseUrl/popular?api_key=fakeApiKey',
-//       )).thenThrow(exception);
+      when(_mockClient.get(
+        '$_baseUrl/popular?api_key=fakeApiKey',
+      )).thenThrow(exception);
 
-//       _fakeMovieFetcher.fetchMovies().listen(expectAsync1((event) => expect(
-//             event,
-//             MoviesResponse.unexpectedException(exception: exception),
-//           )));
-//     });
-//   });
-// }
+      _fakeMovieFetcher.fetchMovies().listen(expectAsync1((event) => expect(
+            event,
+            MoviesResponse.unexpectedException(exception: exception),
+          )));
+    });
+  });
+}
--- super_enum/lib/src/annotations.dart
+++ super_enum/lib/src/annotations.dart
@@ -31,30 +31,9 @@ class Generic {
 @immutable
 class DataField<T> {
   final String name;
+
   const DataField(this.name);
 }
-
-//
-//@immutable
-//class DataField extends Field {
-//  final Type type;
-//  const DataField(final String name, this.type) : super(name);
-//}
-//
-//@immutable
-//class DataList extends Field {
-//  final Type intrinsicType;
-//
-//  const DataList(String name, this.intrinsicType) : super(name);
-//}
-//
-//@immutable
-//class DataMap extends Field {
-//  final Type keyType;
-//  final Type valueType;
-//
-//  const DataMap(String name, this.keyType, this.valueType) : super(name);
-//}
 
 @immutable
 class UseClass {
